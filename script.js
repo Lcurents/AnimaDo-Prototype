1b8dbb7237988ecbf508940a43ed62e1e3d6c118
dika <laurentiusdika28@gmail.com>
2025-12-12 21:20:19 +0700

    Refactor timer functionality to display minutes and seconds, updating HTML structure accordingly

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
-let waktu = 10;
+let minute = 25;
+let sec = 0;
 let timer;
+let running = false;
+
 function play() {
-    timer = setInterval(() => {
-        document.getElementById("waktu").innerHTML = waktu;
-        console.log(waktu);
-        waktu--;
-        if (waktu < 0) {
-            clearInterval(timer);
-            console.log("Waktu Habis")
-        }
-    }, 1000
-    );
+    if (running == false) {
+        running = true;
+        console.log(running)
+        timer = setInterval(() => {
+            upHTML();
+            if (sec < 0) {
+                if (minute == 0 && sec == 0) {
+                    running = false;
+                    console.log(running)
+                    clearInterval(timer);
+                }
+                sec = 59;
+
+            }
+            if (sec == 59) {
+                minute--;
+                upHTML();
+                sec--;
+            } else {
+                sec--;
+            }
+        }, 1000
+        );
+    }
 }
 
 function reset() {
-    waktu = 10;
-    document.getElementById("waktu").innerHTML = waktu;
+    sec = 0;
+    minute = 25;
+    clearInterval(timer);
+    running = false;
+    upHTML();
+}
 
+function pause() {
+    clearInterval(timer);
+    running = false;
+}
+
+function upHTML() {
+    if (sec < 10) {
+        document.getElementById("sec").innerHTML = "0" + sec;
+    } else {
+        document.getElementById("sec").innerHTML = sec;
+    }
+    document.getElementById("minute").innerHTML = minute;
 }
